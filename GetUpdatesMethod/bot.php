b2439f840f68ed5aa497caf04482b648308f7a9b
T568A <andrey.lopatkin@outlook.com>
2016-11-20 20:18:28 +0500

    add config

--- GetUpdatesMethod/bot.php
+++ GetUpdatesMethod/bot.php
@@ -6,4 +6,6 @@ use Bot\App\Update;
 
 require_once __DIR__ . '/bootstrap/autoload.php';
 
-var_dump((new Update("https://api.telegram.org/bot<token>/getUpdates"))->getUpdates());
+$config = require_once __DIR__ . '/config.php';
+
+var_dump((new Update('https://api.telegram.org/bot' . $config['token'] . '/getUpdates'))->getUpdates());
