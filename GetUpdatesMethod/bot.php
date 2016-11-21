b7dcdd43e62ee0cbce1ac49f7ae93f000ccf9a6e
T568A <andrey.lopatkin@outlook.com>
2016-11-21 14:59:57 +0500

    add loop

--- GetUpdatesMethod/bot.php
+++ GetUpdatesMethod/bot.php
@@ -8,19 +8,29 @@ require_once __DIR__ . '/bootstrap/autoload.php';
 
 $config = require_once __DIR__ . '/config.php';
 
+$updateId = 0;
+
 try {
-    $obj = (new GetMessage('https://api.telegram.org/bot' . $config['token'] . '/getUpdates'))->getUpdates();
-    if (is_bool($obj->ok) && $obj->ok) {
-        var_dump($obj);
-        foreach ($obj->result as $value) {
-            echo 'chatID: ' . $value->message->chat->id . PHP_EOL;
-            echo 'name: ' . $value->message->from->first_name . PHP_EOL;
-            echo 'text: ' . $value->message->text . PHP_EOL;
-            echo 'date: ' . $value->message->date . PHP_EOL;
-            echo '-----------------------' . PHP_EOL;
+    while (true) {
+        $obj = (new GetMessage('https://api.telegram.org/bot' . $config['token'] . '/getUpdates?offset=' . $updateId))->getUpdates();
+        if (is_bool($obj->ok) && $obj->ok) {
+            //var_dump($updateId);
+            $updateId = (!empty($obj->result)) ? (end($obj->result))->update_id + 1 : $updateId;
+            //var_dump($updateId);
+            //var_dump($obj);
+            foreach ($obj->result as $value) {
+                echo 'chatID: ' . $value->message->chat->id . PHP_EOL;
+                echo 'name: ' . $value->message->from->first_name . PHP_EOL;
+                echo 'text: ' . $value->message->text . PHP_EOL;
+                echo 'date: ' . $value->message->date . PHP_EOL;
+                echo 'update_id: ' . $value->update_id . PHP_EOL;
+
+                echo '-----------------------' . PHP_EOL;
+            }
+        }  else {
+            throw new \Exception('FAIL!(main)');
         }
-    } else {
-        throw new \Exception('FAIL!(main)');
+        sleep(15);
     }
 } catch (Exception $e) {
     echo 'Error: ' . $e->getMessage() . PHP_EOL;
